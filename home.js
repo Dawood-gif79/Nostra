383bd02c0ecc8495777947f994d27df55ca0486b
Dawood-gif79 <Dawood232003@gmail.com>
2025-01-13 19:27:13 +0530

    Nostra

---
+++ home.js
@@ -0,0 +1,129 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var offerBar = document.querySelector(".offer-bar");
+    var closeButton = document.getElementById("offer-close");
+
+    // Debug logs
+    console.log("Offer Bar:", offerBar);
+    console.log("Close Button:", closeButton);
+
+    // Check if elements exist
+    if (offerBar && closeButton) {
+        closeButton.addEventListener("click", function () {
+            console.log("Close button clicked!");
+            offerBar.style.display = "none"; // Hide the offer bar
+        });
+    } else {
+        console.error("Element not found!");
+    }
+});
+
+var sideNavMenu=document.getElementById("side-navbar-activate")
+var sidenavbar = document.querySelector(".side-navbar")
+sideNavMenu.addEventListener("click",function(){
+    sidenavbar.style.marginLeft="0px"
+})
+
+document.getElementById("side-navbar-close").addEventListener("click",()=>{
+    document.querySelector(".side-navbar").style.marginLeft = "-60%"
+})
+
+var sideNavMenu=document.getElementById("side-navbar-activate")
+var sidenavbar = document.querySelector(".side-navbar")
+sideNavMenu.addEventListener("click",function(){
+    sidenavbar.style.marginLeft="0px"
+})
+
+document.getElementById("side-navbar-close").addEventListener("click",()=>{
+    document.querySelector(".side-navbar").style.marginLeft = "-60%"
+})
+
+
+
+var sliderleftbutton = document.getElementById("slider-left-activate")
+var sliderrightbutton =document.getElementById("slider-right-activate")
+var sliderimage= document.querySelector(".slider-image-container")
+var slidermargin=0
+
+console.log(sliderleftbutton)
+
+sliderrightbutton.addEventListener("click",
+
+function(){
+    slidermargin=slidermargin+100
+
+    if(slidermargin>200)
+    {
+        slidermargin=0
+        sliderimage.style.marginLeft=0;
+    }
+    else{
+        sliderimage.style.marginLeft="-"+slidermargin+"vw";
+    }
+    
+   
+
+}
+)
+
+sliderleftbutton.addEventListener("click",
+
+function(){
+
+    if(slidermargin==0)
+    {
+       
+        slidermargin=200
+        sliderimage.style.marginLeft="-"+slidermargin+"vw";
+    }
+    else{
+
+        slidermargin=slidermargin-100
+    sliderimage.style.marginLeft="-"+slidermargin+"vw";
+    }
+    
+    
+}
+)
+
+
+
+
+
+var likebuttons=document.querySelectorAll(".like-button")
+
+likebuttons.forEach((btn)=>{
+    btn.addEventListener("click",function(e){
+        
+        
+        if(e.target.src.indexOf("blackheart")>0)
+        {
+            console.log("jkjk");
+            e.target.src="img/icons/redheart.png"
+        }
+        else{
+            e.target.src="img/icons/blackheart.png"
+        }
+    })
+})
+
+window.addEventListener("scroll",function(){
+    var elements = this.document.querySelectorAll(".initial-scroll-animate")
+    elements.forEach((el)=>{
+        windowHeight = window.innerHeight
+     var elbound = el.getBoundingClientRect()
+   
+
+     console.log(windowHeight)
+     console.log(elbound.top)
+     if(windowHeight>elbound.top-100){
+        console.log("Hi")
+    el.classList.remove("reveal-scroll-animate")
+
+    }
+    
+})
+    
+})
+
+
+//Collections
